8ceeafca2d1e11a9ec90fc847c55d1a1cac196b3
christhopermarcelino <christhopermarcelino@gmail.com>
2024-08-31 16:36:47 +0700

    feat: add order routes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth-jwt', 'api'])->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/me', function() {
-        return response()->json(auth()->user(), 200);
-    });
+    Route::get('/orders/report', [OrderController::class, 'report']);
+    Route::resource('/orders', OrderController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
