7557994628be2ad421533c1cc395798e4ec175f7
christhopermarcelino <christhopermarcelino@gmail.com>
2024-09-01 23:25:02 +0700

    feat: require token in create, update, and delete category and product

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,15 @@ Route::get('/', function() {
 
 Route::middleware(['auth-jwt', 'api'])->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::resource('/categories', CategoryController::class)->except(['index', 'show']);
+    Route::resource('/products', ProductController::class)->except(['index', 'show']);
+
     Route::get('/orders/report', [OrderController::class, 'report']);
     Route::resource('/orders', OrderController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::resource('/categories', CategoryController::class);
-Route::resource('/products', ProductController::class);
+Route::resource('/categories', CategoryController::class)->only(['index', 'show']);
+Route::resource('/products', ProductController::class)->only(['index', 'show']);
